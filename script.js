2dc54ec6f85744ea8f3780a95b49de57d17aea3d
TheMightyRaider <sivasanjay7@gmail.com>
2020-03-02 19:55:31 +0530

    Added Drag/Drop,Enhanced UI

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const button = document.querySelector("button");
 const input = document.querySelector('input[type="file"]');
 let fileName = document.querySelector(".fileName");
 const preview = document.querySelector(".preview");
+const dropBox = document.querySelector(".dropbox");
+const header = document.querySelector(".header");
 
 const fileDetails = input.files;
+let fileCollection = [];
+let fileList = [];
+let itemToBeDisplayed = [];
 
-function handleClick(event) {
-  fileName.remove();
+function handleClick() {
+  // header.innerHTML = "";  NOT WORKING ! WHY ?
+  itemToBeDisplayed = [...new Set(fileList)];
+  console.log(itemToBeDisplayed[0] === itemToBeDisplayed[1]);
   for (const files of fileDetails) {
     const img = document.createElement("img");
     imageURL = URL.createObjectURL(files);
@@ -16,11 +23,38 @@ function handleClick(event) {
   }
 }
 
-function displayitem() {
-  fileName.insertAdjacentHTML("afterbegin", "<b>Files to be Uploaded</b>");
-  for (const item of fileDetails) {
-    fileName.insertAdjacentHTML("beforeend", `<p>${item.name}</p>`);
+function handleEvent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function checkDuplicate(file) {
+  console.log(fileCollection.length);
+  return fileCollection.length == 0 ? false : fileCollection.includes(file);
+}
+
+function displayitem(files = fileDetails) {
+  header.innerHTML = "<b>Files to be Uploaded</b>";
+  for (const item of files) {
+    const result = checkDuplicate(item.name);
+    result ? null : (fileName.innerHTML += `<p class='files'>${item.name}</p>`);
+    fileCollection.push(item.name);
+    fileList.push(item);
   }
 }
 
+function getFileInfo(event) {
+  const data = event.dataTransfer;
+  files = data.files;
+  displayitem(files);
+}
+
 button.addEventListener("click", handleClick);
+
+// Adding drag and drop event listener to the dropBox
+
+["dragenter", "dragover", "drop"].forEach(item => {
+  dropBox.addEventListener(item, handleEvent, false);
+});
+
+dropBox.addEventListener("drop", getFileInfo, false);
