7e254d7ad7691b3eeece3ff6df4ad2f124cac739
TheMightyRaider <sivasanjay7@gmail.com>
2020-03-03 13:32:58 +0530

    Finished Level-2

---
+++ lib.js
@@ -0,0 +1,63 @@
+const lib = (function() {
+  //IIFE
+  function appendImage(file) {
+    const preview = document.querySelector(".preview");
+    const img = document.createElement("img");
+    const imageURL = URL.createObjectURL(file);
+    img.src = imageURL;
+    img.classList.add("image");
+    preview.appendChild(img);
+  }
+
+  function appendFileName(fileName) {
+    const header = document.querySelector(".header");
+    let fileNameContainer = document.querySelector(".fileName");
+    header.innerHTML = "<b>Files to be Uploaded</b>";
+    fileNameContainer.innerHTML += `<p class='files'>${fileName}</p>`;
+  }
+
+  function clearFileNames() {
+    document.querySelector('input[name="uploadFile"]').value = "";
+    document.querySelector(".header").innerHTML = "";
+    document.querySelectorAll(".files").forEach(item => item.remove());
+  }
+
+  function addDisplayHandler(handler) {
+    const button = document.querySelector("button");
+    button.addEventListener("click", handler);
+  }
+
+  function stopDefaults(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function addDropHandler(handler) {
+    const dropBox = document.querySelector(".dropbox");
+    ["dragenter", "dragover", "drop"].forEach(item => {
+      dropBox.addEventListener(item, stopDefaults, false);
+    });
+
+    dropBox.addEventListener("drop", handler, false);
+  }
+
+  //   function getButton(){
+  //       return document.querySelector('button')
+  //   }
+
+  /*METHOD 1: 
+    lib.getbutton().addEventListener(handler);
+
+    METHOD 2:
+    lib.addDropEventListener(handler);
+    lib.addDisplayHandler(d)
+  */
+
+  return {
+    appendImage,
+    appendFileName,
+    clearFileNames,
+    addDisplayHandler,
+    addDropHandler
+  };
+})();
--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-const button = document.querySelector("button");
-const input = document.querySelector('input[type="file"]');
-let fileName = document.querySelector(".fileName");
-const preview = document.querySelector(".preview");
-const dropBox = document.querySelector(".dropbox");
-const header = document.querySelector(".header");
-
+const input = document.querySelector('input[name="uploadFile"]');
 const fileDetails = input.files;
 let fileCollection = [];
 let fileList = [];
 let itemToBeDisplayed = [];
 
-function handleClick() {
-  // header.innerHTML = "";  NOT WORKING ! WHY ?
-  itemToBeDisplayed = [...new Set(fileList)];
-  console.log(itemToBeDisplayed[0] === itemToBeDisplayed[1]);
-  for (const files of fileDetails) {
-    const img = document.createElement("img");
-    imageURL = URL.createObjectURL(files);
-    img.src = imageURL;
-    img.classList.add("image");
-    preview.insertAdjacentElement("beforeend", img);
+function displayImage() {
+  lib.clearFileNames();
+  console.log(fileList);
+  for (const file of fileList) {
+    lib.appendImage(file);
   }
-}
-
-function handleEvent(event) {
-  event.preventDefault();
-  event.stopPropagation();
+  fileList = [];
 }
 
 function checkDuplicate(file) {
-  console.log(fileCollection.length);
-  return fileCollection.length == 0 ? false : fileCollection.includes(file);
+  return fileList.map(item => item.name).includes(file);
 }
 
 function displayitem(files = fileDetails) {
-  header.innerHTML = "<b>Files to be Uploaded</b>";
   for (const item of files) {
-    const result = checkDuplicate(item.name);
-    result ? null : (fileName.innerHTML += `<p class='files'>${item.name}</p>`);
-    fileCollection.push(item.name);
-    fileList.push(item);
+    const isFileValid = checkDuplicate(item.name);
+    if (!isFileValid) {
+      lib.appendFileName(item.name);
+      fileList.push(item); //Single source of truth
+    }
   }
 }
 
@@ -49,12 +33,5 @@ function getFileInfo(event) {
   displayitem(files);
 }
 
-button.addEventListener("click", handleClick);
-
-// Adding drag and drop event listener to the dropBox
-
-["dragenter", "dragover", "drop"].forEach(item => {
-  dropBox.addEventListener(item, handleEvent, false);
-});
-
-dropBox.addEventListener("drop", getFileInfo, false);
+lib.addDisplayHandler(displayImage);
+lib.addDropHandler(getFileInfo);
