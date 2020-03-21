8b64cc7c1b8847ec7236415fb01d7e80d6fd3b8f
TheMightyRaider <sivasanjay7@gmail.com>
2020-03-21 23:29:57 +0530

    Organizied E2E Testing for ImageUploader

--- frontend.test.js
+++ frontend.test.js
@@ -1,255 +1,240 @@
 const puppeteer = require("puppeteer");
 
-const app = "http://127.0.0.1:5500/index.html";
+const app = process.env.URL || "http://127.0.0.1:5500/index.html";
 let browser;
 let page;
+let fileInput;
 
-beforeEach(async () => {
+beforeAll(async () => {
   browser = await puppeteer.launch({
-    // headless: false,
-    // slowMo: 100
+    headless: false,
+    slowMo: 100
   });
   page = await browser.newPage();
   await page.goto(app, {
     waitUntil: "domcontentloaded"
   });
+  await page.waitForSelector('input[type="file"]');
+  fileInput = await page.$('input[name="files"]');
 });
 
-test("Checking for File Input", async () => {
-  await page.waitForSelector('input[type="file"]');
-  const fileInput = await page.$('input[name="files"]');
-
-  // Checking if the file input exists
-
-  expect(fileInput).toBeTruthy();
-
-  await fileInput.uploadFile(
-    "/home/sanjay/Pictures/discord.jpg",
-    "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
-  );
-
-  fileInput.evaluate(element => {
-    const event = new Event("change");
-    element.dispatchEvent(event);
+describe("Checking for File Input", () => {
+  test("Check if the file input exists", async () => {
+    expect(fileInput).toBeTruthy();
   });
 
-  await page.waitForSelector("#header");
-  const fileHeader = await page.$("#header");
-  const fileSelectedHeader = await fileHeader.evaluate(
-    element => element.innerText
-  );
+  test("Check if the file header is displayed", async () => {
+    await fileInput.uploadFile(
+      "/home/sanjay/Pictures/discord.jpg",
+      "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
+    );
 
-  // Checking if the file has been selected and displayed
+    fileInput.evaluate(element => {
+      const event = new Event("change");
+      element.dispatchEvent(event);
+    });
 
-  expect(fileSelectedHeader).toBe("File Selected");
+    await page.waitForSelector("#header");
+    const fileHeader = await page.$("#header");
+    const fileSelectedHeader = await fileHeader.evaluate(
+      element => element.innerText
+    );
 
-  //check for fileName being displayed
+    expect(fileSelectedHeader).toBe("File Selected");
+  });
 
-  await page.waitForSelector(".fileName");
-  const fileName = await page.$(".fileName");
-  expect(fileName).toBeTruthy();
+  test("check if the file name is displayed", async () => {
+    await page.waitForSelector(".fileName");
+    const fileName = await page.$(".fileName");
+    expect(fileName).toBeTruthy();
+  });
 
-  //check if the remove element exists
+  test("Check if the remove button exists ", async () => {
+    await page.waitForSelector(".remove");
+    const removeButton = await page.$(".remove");
+    expect(removeButton).toBeTruthy();
+  });
 
-  await page.waitForSelector(".remove");
-  const removeButton = await page.$(".remove");
-  expect(removeButton).toBeTruthy();
+  test("Check if removes remove the filename", async () => {
+    await page.click(".remove");
+    const removeFirstImage = await page.$(".remove");
+    expect(removeFirstImage).toBeTruthy();
+  });
 
-  // Removing the first Image
-
-  await page.click(".remove");
-
-  //Check if there is a single image to be uploaded exist
-
-  const removeFirstImage = await page.$(".remove");
-  expect(removeFirstImage).toBeTruthy();
-
-  // Remove the last image
-
-  await page.click(".remove");
-
-  // Check if the element doesn't exist after removed
-
-  const afterClicked = await page.$(".remove");
-  expect(afterClicked).toBeFalsy();
-
-  await browser.close();
+  test("Check if all the elements have been removed", async () => {
+    await page.click(".remove");
+    const afterClicked = await page.$(".remove");
+    expect(afterClicked).toBeFalsy();
+  });
 });
 
-test("Uploading a file, After the Files have been chosen", async () => {
-  // Checking if the upload is disabled when no file is selected
-
-  await page.waitForSelector("#b1");
-  const uploadButton = await page.$("#b1");
-  const isDisabled = await uploadButton.evaluate(element => element.disabled);
-  expect(isDisabled).toBeTruthy();
-
-  //Upload the Files
-
-  await page.waitForSelector('input[type="file"]');
-  const inputFile = await page.$('input[type="file"]');
-
-  await inputFile.uploadFile(
-    "/home/sanjay/Pictures/discord.jpg",
-    "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
-  );
-
-  // Triggering the change event
-
-  inputFile.evaluate(element => {
-    let event = new Event("change");
-    element.dispatchEvent(event);
+describe("Uploading a file, After the Files have been chosen", async () => {
+  test("Checking if the upload is disabled when no file is selected", async () => {
+    await page.waitForSelector("#b1");
+    const uploadButton = await page.$("#b1");
+    const isDisabled = await uploadButton.evaluate(element => element.disabled);
+    expect(isDisabled).toBeTruthy();
   });
 
-  // Checking if the upload button is enabled afer selecting files
+  test("Test if the upload button is enabled after the files are choosen", async () => {
+    await page.waitForSelector('input[type="file"]');
+    const inputFile = await page.$('input[type="file"]');
 
-  await page.waitForSelector("#b1");
-  const afteruploadButton = await page.$("#b1");
-  const isEnabled = await afteruploadButton.evaluate(
-    element => element.disabled
-  );
-  expect(isEnabled).toBeFalsy();
+    await inputFile.uploadFile(
+      "/home/sanjay/Pictures/discord.jpg",
+      "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
+    );
 
-  // Uploading the Files to Local Storage
+    // Triggering the change event
 
-  await page.waitForSelector("#b1");
-  await page.click("#b1");
+    inputFile.evaluate(element => {
+      let event = new Event("change");
+      element.dispatchEvent(event);
+    });
 
-  // Checking if the file has been uploaded and the uploaded header is displayed
+    // Checking if the upload button is enabled afer selecting files
 
-  await page.waitForSelector(".removeHeader");
-  const uploadedHeader = await page.evaluate(
-    () => document.querySelector(".removeHeader").innerText
-  );
-  expect(uploadedHeader).toBe("Uploaded!");
+    await page.waitForSelector("#b1");
+    const afteruploadButton = await page.$("#b1");
+    const isEnabled = await afteruploadButton.evaluate(
+      element => element.disabled
+    );
+    expect(isEnabled).toBeFalsy();
+  });
 
-  // Check if the images is be displayed
+  test("Check if the upload header is displayed", async () => {
+    await page.waitForSelector("#b1");
+    await page.click("#b1");
+    await page.waitForSelector(".removeHeader");
+    const uploadedHeader = await page.evaluate(
+      () => document.querySelector(".removeHeader").innerText
+    );
+    expect(uploadedHeader).toBe("Uploaded!");
+  });
 
-  await page.waitForSelector("img");
-  const uploadedImage = await page.evaluate(() =>
-    document.querySelector("img")
-  );
-  expect(uploadedImage).toBeTruthy();
+  test("Check if the images are being displayed", async () => {
+    await page.waitForSelector("img");
+    const uploadedImage = await page.evaluate(() =>
+      document.querySelector("img")
+    );
+    expect(uploadedImage).toBeTruthy();
+  });
 
   // Check if the upload button is disabled
-
-  await page.waitForSelector("#b1");
-  expect(
-    await page.evaluate(_ => document.querySelector("#b1").disabled)
-  ).toBeTruthy();
-  await browser.close();
+  test("Check if the upload button is disabled after upload", async () => {
+    await page.waitForSelector("#b1");
+    expect(
+      await page.evaluate(_ => document.querySelector("#b1").disabled)
+    ).toBeTruthy();
+  });
 }, 10000);
 
-test("After Upload but before Refresh, Test for Removing a Image", async () => {
+describe("After Upload but before Refresh, Test for Removing a Image", async () => {
   // Uploading the Files
+  test("Check if the image is removed on click", async () => {
+    await fileInput.uploadFile(
+      "/home/sanjay/Pictures/discord.jpg",
+      "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
+    );
 
-  await page.waitForSelector('input[type="file"]');
-  const inputFile = await page.$('input[type="file"]');
+    fileInput.evaluate(element => {
+      let event = new Event("change");
+      element.dispatchEvent(event);
+    });
 
-  await inputFile.uploadFile(
-    "/home/sanjay/Pictures/discord.jpg",
-    "/home/sanjay/Pictures/landscape-mountains-firewatch-silhouette-wallpaper.png"
-  );
+    await page.waitForSelector("#b1");
+    await page.click("#b1");
 
-  inputFile.evaluate(element => {
-    let event = new Event("change");
-    element.dispatchEvent(event);
+    // Check to remove first image on click
+
+    const imageClicked = await page.evaluate(_ =>
+      document.querySelectorAll("img")[0].click()
+    );
+
+    expect(imageClicked).toBeFalsy();
   });
-
-  await page.waitForSelector("#b1");
-  await page.click("#b1");
-
-  // Check to remove first image on click
-
-  const imageClicked = await page.evaluate(_ =>
-    document.querySelectorAll("img")[0].click()
-  );
-
-  expect(imageClicked).toBeFalsy();
 
   //Check whether the second image exist
-
-  const secondImage = await page.$("img");
-  expect(secondImage).toBeTruthy();
-
-  //Remove the second image
-
-  const removeSecondImage = await page.evaluate(_ =>
-    document.querySelector("img").click()
-  );
-  expect(removeSecondImage).toBeFalsy();
-
-  //Check whether the uploadHeader has been removed!
-
-  await page.waitForSelector(".removeHeader");
-  const uploadHeader = await page.evaluate(
-    () => document.querySelector(".removeHeader").innerText
-  );
-
-  expect(uploadHeader).toBe("");
-
-  await browser.close();
-});
-
-test("After Upload, Test to remove image", async () => {
-  // Uploading the Files
-
-  await page.waitForSelector('input[type="file"]');
-  const inputFile = await page.$('input[type="file"]');
-
-  await inputFile.uploadFile("/home/sanjay/Pictures/discord.jpg");
-
-  inputFile.evaluate(element => {
-    let event = new Event("change");
-    element.dispatchEvent(event);
+  test("Check if the second image is displayed", async () => {
+    const secondImage = await page.$("img");
+    expect(secondImage).toBeTruthy();
   });
 
-  await page.waitForSelector("#b1");
-  await page.click("#b1");
+  test("Check if the all the images are removed", async () => {
+    const removeSecondImage = await page.evaluate(_ =>
+      document.querySelector("img").click()
+    );
+    expect(removeSecondImage).toBeFalsy();
+  });
 
-  // Reloading the Page, After Uploading the Files
+  //Check whether the uploadHeader has been removed!
+  test("Check if the uploaderHeader is empty", async () => {
+    await page.waitForSelector(".removeHeader");
+    const uploadHeader = await page.evaluate(
+      () => document.querySelector(".removeHeader").innerText
+    );
 
-  await page.reload();
+    expect(uploadHeader).toBe("");
+  });
+});
 
-  // Check the Refresh Header is displayed
+describe("After Refresh, Test to remove image", async () => {
+  test("Check if the Refresh Header exists after refresh", async () => {
+    await page.waitForSelector('input[type="file"]');
+    const inputFile = await page.$('input[type="file"]');
 
-  await page.waitForSelector(".removeHeader");
-  const refreshHeader = await page.evaluate(
-    _ => document.querySelector(".removeHeader").textContent
-  );
+    await inputFile.uploadFile("/home/sanjay/Pictures/discord.jpg");
 
-  expect(refreshHeader).toBe("Click the Image to remove it!");
+    inputFile.evaluate(element => {
+      let event = new Event("change");
+      element.dispatchEvent(event);
+    });
 
-  // Check if the img exists
+    await page.waitForSelector("#b1");
+    await page.click("#b1");
 
-  await page.waitForSelector("img");
-  const onRefreshImage = await page.$("img");
+    // Reloading the Page, After Uploading the Files
 
-  expect(onRefreshImage).toBeTruthy();
+    await page.reload();
 
-  // Check if the image is removed after click
+    // Check the Refresh Header is displayed
 
-  const imageClicked = await page.evaluate(_ =>
-    document.querySelector("img").click()
-  );
+    await page.waitForSelector(".removeHeader");
+    const refreshHeader = await page.evaluate(
+      _ => document.querySelector(".removeHeader").textContent
+    );
 
-  expect(imageClicked).toBeFalsy();
+    expect(refreshHeader).toBe("Click the Image to remove it!");
+  });
 
-  // Check if the header is removed
+  test("Check if the images are displayed on refresh", async () => {
+    await page.waitForSelector("img");
+    const onRefreshImage = await page.$("img");
 
-  await page.waitForSelector(".removeHeader");
-  const headerAfterAllImageHaveBeenRemoved = await page.evaluate(
-    _ => document.querySelector(".removeHeader").textContent
-  );
+    expect(onRefreshImage).toBeTruthy();
+  });
 
-  expect(headerAfterAllImageHaveBeenRemoved).toBe("");
+  test("Check if the image is removed when clicked", async () => {
+    const imageClicked = await page.evaluate(_ =>
+      document.querySelector("img").click()
+    );
 
-  // Check if the image is removed from the localStorage
+    expect(imageClicked).toBeFalsy();
+  });
 
-  await page.reload();
+  test("Check if the header is empty when all images are removed", async () => {
+    await page.waitForSelector(".removeHeader");
+    const headerAfterAllImageHaveBeenRemoved = await page.evaluate(
+      _ => document.querySelector(".removeHeader").textContent
+    );
 
-  const checkIfImageExists = await page.$("img");
-  expect(checkIfImageExists).toBeFalsy();
+    expect(headerAfterAllImageHaveBeenRemoved).toBe("");
+  });
 
-  await browser.close();
+  test("Check if all the images are removed from the DOM, after refresh", async () => {
+    await page.reload();
+    const checkIfImageExists = await page.$("img");
+    expect(checkIfImageExists).toBeFalsy();
+    await browser.close();
+  });
 });
